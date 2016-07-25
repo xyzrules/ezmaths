e2efa9556508168d3cb4aed4ba14e2f02ce8e34a
xyzrules <nvdung5699@gmail.com>
2016-07-25 11:46:58 +0700

    Update lalala.js

--- lalala.js
+++ lalala.js
@@ -1,6 +1,7 @@
-//example input
-var str="3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
-
+/* 
+	input parser "lalala" by xyzrules
+	touched up by harryt 
+*/
 var num="x";
 
 var pos=0;
@@ -9,59 +10,74 @@ var stack = [];
 //output queue
 var queue = [];
 
-for (var i=0; i<str.length;i++){
-	if (str[i]==" ") {}
-	else {
-		if ( isNaN (Number(str[i]) ) === false ){
-			if (num==="x")	num=0;
-			num=num*10+Number(str[i]);
-		}
-		else {
-			check_num();
-			if (str[i]==="x"){
-				queue.push("x");
+var list = ["sin","cos","tan","cot","log","ln"];
+
+function input_parser(str){
+	stack = [];
+	queue = []
+
+	for (var i=0; i<str.length;i++){
+		if (str[i]!=" ") {
+			if ( isNaN (Number(str[i]) ) === false ){
+				if (num==="x")	num=0;
+				num=num*10+Number(str[i]);
 			}
-			else if (is_operator(str[i])!==0){
-				while (stack.length>0){
-					var res=stack.pop();
-					if (order_operator(str[i],res)===false){
-						stack.push(res);
-						break;
+			else {
+				check_num();
+				if (str[i]==="x" || str[i]==="e"){
+					queue.push(str[i]);
+				}
+				else if (is_operator(str[i])!==0 && is_operator(str[i])!==4){
+					while (stack.length>0){
+						var res=stack.pop();
+						if (order_operator(str[i],res)===false){
+							stack.push(res);
+							break;
+						}
+					}
+					stack.push(str[i]);
+				}
+				else if (str[i]==="("){
+					stack.push(str[i]);
+				}
+				else if (str[i]==")"){
+					var res="x";
+					while (res!=="("){
+						res=stack.pop();
+						if (res!=="(")
+							queue.push(res);
 					}
 				}
-				stack.push(str[i]);
-			}
-			else if (str[i]==="("){
-				stack.push(str[i]);
-			}
-			else if (str[i]==")"){
-				var res="x";
-				while (res!=="("){
-					res=stack.pop();
-					if (res!=="(")
-						queue.push(res);
+				else {
+					//other operator such as sin, cos, tan, cot, log, ln
+					if (str[i]==="l" && str[i+1]==="n"){
+						stack.push("ln");
+					}
+					else {
+						stack.push(str.substring(i,i+3));
+						i=i+2;
+					}
 				}
 			}
+
 		}
-
 	}
+
+
+	while (stack.length!=0){
+		queue.push(stack.pop());
 }
 
-
-while (stack.length!=0){
-	queue.push(stack.pop());
+return to_tree(queue);
 }
-
-while (queue.length>0){
-	console.log(queue.pop());
-}
-
-//insert data into array
 
 function is_operator(data){
+	for (var i=0;i<=5;i++){
+		if (data===list[i])	return 4;
+	}
 	if (data==="^")
 		return 3;
-	else if (data==="*" || data==="/" || data=="%")
+	else if (data==="*" || data==="/" || data==="%")
 		return 2;
 	else if (data==="+" || data==="-")
 		return 1;
@@ -71,19 +87,35 @@ function is_operator(data){
 function check_num(){
 	if (!isNaN(num)){
 		queue.push(num);
-		num="x";		
+		num="x";
 	}
 }
 
 function order_operator(data1, data2){
 	var res1=is_operator(data1);
 	var res2=is_operator(data2);
-	if (res1===3){
-		return false;
+	if (res1>=3 && res2>res1){
+		return true;
 	}
-	else if (res2>=res1){
+	else if (res1<3 && res2>=res1){
 		queue.push(data2);
 		return true;
 	}
 	return false;
+}
+
+
+function to_tree(array){
+	var input_tree = new tree(array[array.length-1]);
+	var current_node = 0
+	for(var i = array.length - 1; i > 0; i -=1){
+			while(input_tree.is_full(current_node)){
+				current_node = input_tree.array_data[current_node].parent
+			}
+			input_tree.add(new node(array[i-1], current_node));
+		if(is_operator(array[i-1]) != 0){
+			current_node = input_tree.array_data.length -1
+		}
+	}
+	input_tree.print_data();
 }
